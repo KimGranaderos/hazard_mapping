376afe408a5d7d4bafc2a39365fdb208bfa56257
KimGranaderos <KimGranaderos09@gmail.com>
2018-10-10 17:28:07 +0800

    home done

--- src/App.vue
+++ src/App.vue
@@ -39,44 +39,44 @@
             <span
              slot="badge"
              small
-            >2</span>
+            >3</span>
             <v-icon>notification_important</v-icon>
           </v-badge>
         </v-btn>
         <v-card>
-            <v-list>
-              <v-list-title><p class="text-xs-center"><b>Quick Respond</b></p></v-list-title>
-              <v-divider></v-divider>
-              <v-list-tile avatar>
+          <v-list>
+            <v-list-title><p class="text-xs-center"><b>Quick Respond</b></p></v-list-title>
+            <v-divider></v-divider>
+              <v-list-tile @click="dialog = !dialog" avatar>
                 <v-list-tile-avatar color="teal">
                   <v-icon dark>pool</v-icon>
                 </v-list-tile-avatar>
                 <v-list-tile-content>
-                  <v-list-tile-title>[Flood] Area-12</v-list-tile-title>
-                  <v-list-tile-sub-title>Gamay 12:00nn Jan/21/2011</v-list-tile-sub-title>
+                  <v-list-tile-title>[Flood] Area-11b (y)</v-list-tile-title>
+                  <v-list-tile-sub-title>Gamay 12:00nn Feb/21/2012</v-list-tile-sub-title>
                 </v-list-tile-content>
               </v-list-tile>
               <v-divider></v-divider>
-              <v-list-tile avatar>
+              <v-list-tile @click="" avatar>
                 <v-list-tile-avatar color="blue-grey">
                   <v-icon dark>beach_access</v-icon>
                 </v-list-tile-avatar>
                 <v-list-tile-content>
-                  <v-list-tile-title>[Storm] Area-11</v-list-tile-title>
-                  <v-list-tile-sub-title>Gimay 10:00am Jan/21/2011</v-list-tile-sub-title>
+                  <v-list-tile-title>[Storm] Area-11b (x)</v-list-tile-title>
+                  <v-list-tile-sub-title>Gimay 11:45am Feb/21/2012</v-list-tile-sub-title>
                 </v-list-tile-content>
               </v-list-tile>
               <v-divider></v-divider>
-              <v-list-tile avatar>
+              <v-list-tile @click="" avatar>
                 <v-list-tile-avatar color="red">
                   <v-icon dark>whatshot</v-icon>
                 </v-list-tile-avatar>
                 <v-list-tile-content>
-                  <v-list-tile-title>[Fire] Area-11</v-list-tile-title>
-                  <v-list-tile-sub-title>Dako 10:00am Nov/01/2010</v-list-tile-sub-title>
+                  <v-list-tile-title>[Fire] Area-13b (y)</v-list-tile-title>
+                  <v-list-tile-sub-title>Dako 10:00am Feb/11/2012</v-list-tile-sub-title>
                 </v-list-tile-content>
               </v-list-tile>
-            </v-list>
+          </v-list>
         </v-card>
       </v-menu>
       <v-menu
@@ -101,10 +101,74 @@
           <v-list>
             <v-list-title><p class="text-xs-center"><b>Brgy. Tanod Tasks</b></p></v-list-title>
             <v-divider></v-divider>
-
+            <v-list-tile two-line avatar>
+              <v-list-tile-avatar color="blue-grey">
+                <v-icon dark>explore</v-icon>
+              </v-list-tile-avatar>
+              <v-list-tile-content>
+                <v-list-tile-title>Tanod Damiel verifying Area-11b (y) </v-list-tile-title>
+                <v-list-tile-sub-title>Flood Quick Respond</v-list-tile-sub-title>
+              </v-list-tile-content>
+              <v-list-tile-action>
+                <v-icon color="blue-grey">
+                  check_circle
+                </v-icon>
+                <v-list-tile-action-text>
+                  10mins ago
+                </v-list-tile-action-text>
+              </v-list-tile-action>
+            </v-list-tile>
+            <v-divider></v-divider>
+            <v-list-tile avatar>
+              <v-list-tile-avatar color="blue-grey">
+                <v-icon dark>explore</v-icon>
+              </v-list-tile-avatar>
+              <v-list-tile-content>
+                <v-list-tile-title>Tanod Maquiling verifying Area-11b (x) </v-list-tile-title>
+                <v-list-tile-sub-title>Storm Quick Respond</v-list-tile-sub-title>
+              </v-list-tile-content>
+              <v-list-tile-action>
+                <v-icon color="blue">
+                  check_circle
+                </v-icon>
+                <v-list-tile-action-text>
+                  25mins ago
+                </v-list-tile-action-text>
+              </v-list-tile-action>
+            </v-list-tile>
           </v-list>
         </v-card>
       </v-menu>
+      <v-dialog v-model="dialog" width="600px">
+        <v-card>
+          <v-layout align-center justify-center row fill-height/>
+            <v-flex>
+              <v-img
+                src="/static/dialogpic.png"
+                width="100%"
+              >
+              </v-img>
+            </v-flex>
+          </v-layout>
+          <v-container class="pa-4">
+            <v-list-tile two-line>
+              <v-list-tile-content class="mr-5">
+                <v-list-tile-title>FLOOD</v-list-tile-title>
+                <v-list-tile-title>Area-11b (y)</v-list-tile-title>
+              </v-list-tile-content>
+              <v-list-tile-content class="ml-5">
+                <v-list-tile-title>Gamay</v-list-tile-title>
+                <v-list-tile-sub-title>Feb-21-2012</v-list-tile-sub-title>
+              </v-list-tile-content>
+            </v-list-tile>
+          </v-container>
+          <v-card-actions>
+          <v-spacer></v-spacer>
+          <p>Quick Respond Team</p>
+          <v-btn color="red" flat @click="dialog = false">Send</v-btn>
+        </v-card-actions>
+          </v-card>
+      </v-dialog>
     </v-toolbar>
     <main>
       <v-content>
@@ -117,6 +181,7 @@
 <script>
   export default {
     data: () => ({
+      dialog: false,
       drawer: null,
       items: [
         { icon: 'dashboard', text: 'Home', link: '/' },
